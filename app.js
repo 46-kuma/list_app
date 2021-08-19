bac6988f1d1a5f12d76f5c26d000724250c6dadc
shirokuma <shirokuma240@gmail.com>
2021-08-19 21:18:09 +0900

    SQL Reconnect

--- app.js
+++ app.js
@@ -6,11 +6,38 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
+  //mysql://b1697e3f9f3791:35c81551@us-cdbr-east-04.cleardb.com/heroku_daa7d2ad0787280?reconnect=true
   host: 'us-cdbr-east-04.cleardb.com',
   user: 'b1697e3f9f3791',
   password: '35c81551',
   database: 'heroku_daa7d2ad0787280'
 });
+
+function handleDisconnect() {
+  console.log('INFO.CONNECTION_DB: ');
+  
+  //connection取得
+  connection.connect(function(err) {
+      if (err) {
+          console.log('ERROR.CONNECTION_DB: ', err);
+          setTimeout(handleDisconnect, 1000);
+      }
+  });
+  
+  //error('PROTOCOL_CONNECTION_LOST')時に再接続
+  connection.on('error', function(err) {
+      console.log('ERROR.DB: ', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          console.log('ERROR.CONNECTION_LOST: ', err);
+          handleDisconnect();
+      } else {
+          throw err;
+      }
+  });
+}
+
+handleDisconnect();
+
 
 var port = process.env.PORT || 5000;
 

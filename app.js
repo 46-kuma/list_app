6be8e2c62f2979598aa463f4e5082bae06ad225b
shirokuma <shirokuma240@gmail.com>
2021-08-19 18:04:30 +0900

    SQL Reconnect

--- app.js
+++ app.js
@@ -14,13 +14,34 @@ const connection = mysql.createConnection({
 
 var port = process.env.PORT || 5000;
 
-connection.connect((err) => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('success');
-});
+var connection;
+
+function handleDisconnect() {
+  console.log('INFO.CONNECTION_DB: ');
+  connection = mysql.createConnection(db_config);
+  
+  //connection取得
+  connection.connect(function(err) {
+    if (err) {
+      console.log('ERROR.CONNECTION_DB: ', err);
+      setTimeout(handleDisconnect, 1000);
+    }
+  });
+  
+  //error('PROTOCOL_CONNECTION_LOST')時に再接続
+  connection.on('error', function(err) {
+    console.log('ERROR.DB: ', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('ERROR.CONNECTION_LOST: ', err);
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
 
 app.get('/', (req, res) => {
 //  res.send('hello'+items[0].name);

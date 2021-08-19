a8d385f003684c2b6b838afa4fc05d3145bac3ba
shirokuma <shirokuma240@gmail.com>
2021-08-19 14:34:06 +0900

    install ejs

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 var mysql = require('mysql');
 const app = express();
 
-//app.use(express.static('public'));
-//app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
   host: 'us-cdbr-east-04.cleardb.com',
@@ -14,70 +14,70 @@ const connection = mysql.createConnection({
 
 var port = process.env.PORT || 5000;
 
-//connection.connect((err) => {
-//  if (err) {
-//    console.log('error connecting: ' + err.stack);
-//    return;
-//  }
-//  console.log('success');
-//});
-
-app.get('/', (req, res) => {
-  res.send('hello'+items[0].name);
-//  res.render('top.ejs');
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
 });
 
-//app.get('/index', (req, res) => {
-//  connection.query(
-//    'SELECT * FROM items',
-//    (error, results) => {
-//      res.render('index.ejs', {items: results});
-//    }
-//  );
-//});
+app.get('/', (req, res) => {
+//  res.send('hello'+items[0].name);
+  res.render('top.ejs');
+});
 
-//app.get('/new', (req, res) => {
-//  res.render('new.ejs');
-//});
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
 
-//app.post('/create', (req, res) => {
-//  connection.query(
-//    'INSERT INTO items (name) VALUES (?)',
-//    [req.body.itemName],
-//    (error, results) => {
-//      res.redirect('/index');
-//    }
-//  );
-//});
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
 
-//app.post('/delete/:id', (req, res) => {
-//  connection.query(
-//    'DELETE FROM items WHERE id = ?',
-//    [req.params.id],
-//    (error, results) => {
-//      res.redirect('/index');
-//    }
-//  );
-//});
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
-//app.get('/edit/:id', (req, res) => {
-//  connection.query(
-//    'SELECT * FROM items WHERE id = ?',
-//    [req.params.id],
-//    (error, results) => {
-//      res.render('edit.ejs', {item: results[0]});
-//    }
-//  );
-//});
+app.post('/delete/:id', (req, res) => {
+  connection.query(
+    'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
-//app.post('/update/:id', (req, res) => {
-//  connection.query(
-//    'UPDATE items SET name=? WHERE id=?',
-//    [req.body.itemName, req.params.id],
-//    (error, results) => {
-//      res.redirect('/index');
-//    }
-//  )
-//});
+app.get('/edit/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.render('edit.ejs', {item: results[0]});
+    }
+  );
+});
+
+app.post('/update/:id', (req, res) => {
+  connection.query(
+    'UPDATE items SET name=? WHERE id=?',
+    [req.body.itemName, req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  )
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
